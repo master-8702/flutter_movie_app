3aa0f75ff355e7d3a42df46bcb2d2f14f83d7880
master-8702 <ibroselman@gmail.com>
2024-04-26 23:56:04 +0300

    add topbar search widget UI

---
+++ lib/constants/movie_category.dart
@@ -0,0 +1,7 @@
+class MovieCategory {
+  static const String popular = 'Popular';
+  static const String upcoming = 'Upcoming';
+  static const String topRated = 'Top Rated';
+  static const String latest = 'Latest';
+  static const String none = '';
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,17 +1,19 @@
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_movie_app/constants/movie_category.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomeScreen extends ConsumerWidget {
   late double _width;
   late double _height;
+  late TextEditingController _searchTextFieldController;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     _width = MediaQuery.of(context).size.width;
     _height = MediaQuery.of(context).size.height;
+    _searchTextFieldController = TextEditingController();
 
     return _buildUI();
   }
@@ -26,6 +28,7 @@ class HomeScreen extends ConsumerWidget {
           alignment: Alignment.center,
           children: [
             _backgroundImage(),
+            _foregroundWidget(),
           ],
         ),
       ),
@@ -53,4 +56,113 @@ class HomeScreen extends ConsumerWidget {
     );
   }
 
+  Widget _foregroundWidget() {
+    return Container(
+      padding: EdgeInsets.only(top: _height * 0.02),
+      width: _width * 0.88,
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          _topBar(),
+        ],
+      ),
+    );
+  }
+
+  Widget _topBar() {
+    return Container(
+      height: _height * 0.08,
+      decoration: BoxDecoration(
+        color: Colors.black54,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          _searchField(),
+          _categoryDropdown(),
+        ],
+      ),
+    );
+  }
+
+  Widget _searchField() {
+    const _border = InputBorder.none;
+    return Container(
+      width: _width * 0.50,
+      height: _height * 0.05,
+      child: TextField(
+        controller: _searchTextFieldController,
+        onSubmitted: (_input) {},
+        style: const TextStyle(color: Colors.white),
+        decoration: const InputDecoration(
+            focusedBorder: _border,
+            border: _border,
+            prefixIcon: Icon(
+              Icons.search,
+              color: Colors.white24,
+            ),
+            hintStyle: TextStyle(color: Colors.white54),
+            filled: false,
+            fillColor: Colors.white24,
+            hintText: 'Search ...'),
+      ),
+    );
+  }
+
+  Widget _categoryDropdown() {
+    return DropdownButton(
+      dropdownColor: Colors.black38,
+      value: MovieCategory.latest,
+      icon: const Icon(
+        Icons.menu,
+        color: Colors.white24,
+      ),
+      underline: Container(
+        height: 1,
+        color: Colors.white24,
+      ),
+      items: const [
+        DropdownMenuItem(
+          value: MovieCategory.latest,
+          child: Text(
+            MovieCategory.latest,
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        DropdownMenuItem(
+          value: MovieCategory.popular,
+          child: Text(
+            MovieCategory.popular,
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        DropdownMenuItem(
+          value: MovieCategory.topRated,
+          child: Text(
+            MovieCategory.topRated,
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        DropdownMenuItem(
+          value: MovieCategory.upcoming,
+          child: Text(
+            MovieCategory.upcoming,
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        DropdownMenuItem(
+          value: MovieCategory.upcoming,
+          child: Text(
+            MovieCategory.none,
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ],
+      onChanged: (_value) {},
+    );
+  }
 }
