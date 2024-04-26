04ad65d50993c03c81efa63898c37be4ffa49bd3
master-8702 <ibroselman@gmail.com>
2024-04-26 20:17:32 +0300

    add background to main screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,16 +1,56 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomeScreen extends ConsumerWidget {
-  const HomeScreen({super.key});
+  late double _width;
+  late double _height;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    _width = MediaQuery.of(context).size.width;
+    _height = MediaQuery.of(context).size.height;
+
     return _buildUI();
   }
-}
 
-Widget _buildUI() {
-  return const Scaffold();
+  Widget _buildUI() {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Container(
+        width: _width,
+        height: _height,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            _backgroundImage(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _backgroundImage() {
+    return Container(
+      width: _width,
+      height: _height,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        image: DecorationImage(
+          image: NetworkImage(
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSKiAdLbAUFESx_QtMnn77aRd6TeezDMvfutBbG547FA&s'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 15.0, sigmaY: 15.0),
+        child: Container(
+          decoration: BoxDecoration(color: Colors.black.withOpacity(0.2)),
+        ),
+      ),
+    );
+  }
+
 }
